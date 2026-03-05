6af8403bf4b5afc488c0810842ea3c75b7873cba
roy6732856 <roy6732856@gmail.com>
2026-03-05 21:42:25 +1300

    feat: Implement `/api/submit` route to send form data to an N8N webhook, configured via environment variables.

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"y+46C/Ikz8Hzze+JQumBMHMZceTrLVfdy5VqF9bxOtY=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"52EAQvw/qvVlhs4buUV7TcpZ2XrfVgvHsbeU22ugWFM=\"\n}"
